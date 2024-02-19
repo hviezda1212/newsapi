52f2cc0c08fd02b86e29c78201c54792dba63366
KIMMIJU <hviezda1212@gmail.com>
2024-02-19 23:16:10 +0900

    edit

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 const API_KEY = `ac9a72663e6c47d28d7d39f05961835f`;
-let news = []
+let newsList = [];
 const getLatestNews = async () => {
-  const url = new URL(
-    `https://noonanewsapi.netlify.app`
-  );
+  const url = new URL(`https://noonanewsapi.netlify.app/top-headlines?`);
   const response = await fetch(url);
-  const data = await response.json()
-  news = data.articles
-  console.log("news",news)
+  const data = await response.json();
+  newsList = data.articles;
+  render();
+  console.log("news", newsList);
+};
+
+const render = () => {
+  const newsHTML = newsList.map(
+    (news) => `        <div class="row news">
+  <div class="col-lg-4">
+    <img class="news-img" src=${news.urlToImage} />
+  </div>
+  <div class="col-lg-8">
+    <h2>${news.title}</h2>
+    <p>${news.description}</p>
+    <div>${news.source.name} * ${news.publishedAt}</div>
+  </div>
+</div>`
+  );
+
+  document.getElementById("news-board").innerHTML = newsHTML;
 };
 
 getLatestNews();
