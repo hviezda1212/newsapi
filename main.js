b2f6dfe4f2efe205195c51d62747c480e5f6eeec
KIMMIJU <hviezda1212@gmail.com>
2024-02-22 20:19:51 +0900

    task 9~13

--- main.js
+++ main.js
@@ -1,21 +1,70 @@
 const API_KEY = `ac9a72663e6c47d28d7d39f05961835f`;
+let url = new URL(`https://noonanewsapi.netlify.app/top-headlines?`);
 let newsList = [];
 let articles = [];
 let page = 1;
 let totalPage = 1;
 
+const menus = document.querySelectorAll(".menus button");
+menus.forEach((menu) =>
+  menu.addEventListener("click", (event) => getNewsByCategory(event))
+);
+
+let userInput = document.getElementById("search-input");
+userInput.addEventListener("keydown", (event) => {
+  if (event.keyCode === 13) {
+    getNewsByKeyword();
+  }
+});
+
+const getNews = async () => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    if (response.status === 200) {
+      if (data.articles.length == 0) {
+        throw new Error("No result for this search");
+      }
+      newsList = data.articles;
+      render();
+    } else {
+      throw new Error(data.message);
+    }
+  } catch (error) {
+    console.log("error", error.message);
+    errorRender(error.message);
+  }
+};
+
 const getLatestNews = async () => {
-  const url = new URL(`https://noonanewsapi.netlify.app/top-headlines?`);
-  const response = await fetch(url);
-  const data = await response.json();
-  newsList = data.articles;
-  render();
-  console.log("news", newsList);
+  url = new URL(`https://noonanewsapi.netlify.app/top-headlines?`);
+  getNews();
+};
+
+const getNewsByCategory = async (event) => {
+  const category = event.target.textContent.toLowerCase();
+  if (category == "news") {
+    return getLatestNews();
+  }
+  url = new URL(
+    `https://noonanewsapi.netlify.app/top-headlines?category=${category}`
+  );
+  getNews();
+};
+
+const getNewsByKeyword = async () => {
+  const keyword = document.getElementById("search-input").value;
+  if (keyword == "") {
+    return getLatestNews();
+  }
+  url = new URL(`https://noonanewsapi.netlify.app/top-headlines?q=${keyword}`);
+  getNews();
 };
 
 const render = () => {
-  const newsHTML = newsList.map(
-    (news) => `        <div class="row news">
+  const newsHTML = newsList
+    .map(
+      (news) => `        <div class="row news">
   <div class="col-lg-4">
     <img class="news-img" src=${news.urlToImage} />
   </div>
@@ -25,10 +74,18 @@ const render = () => {
     <div>${news.source.name} * ${news.publishedAt}</div>
   </div>
 </div>`
-  ).join("");
+    )
+    .join("");
 
-  console.log("html",newsHTML)
   document.getElementById("news-board").innerHTML = newsHTML;
+};
+
+const errorRender = (errorMessage) => {
+  const errorHTML = `<div class="alert alert-danger" role = "alert">
+    ${errorMessage}
+  </div>`;
+
+  document.getElementById("news-board").innerHTML = errorHTML;
 };
 
 const openSearchBox = () => {
@@ -48,4 +105,4 @@ const closeNav = () => {
   document.getElementById("sideNav").style.width = "0";
 };
 
-getLatestNews();
+getNews();
